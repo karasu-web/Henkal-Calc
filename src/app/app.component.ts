3b72019c9fa8f1c1825ec73a0c215b77181d4d1e
Shajid-Henkal <shajid@karasu-online.com>
2024-06-24 12:33:18 +0600

    Romania product images fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,16 @@ import { MatSliderModule } from '@angular/material/slider';
 import { CommonModule } from '@angular/common';
 // import * as translations from '../assets/pl.json';
 // import * as translations from '../assets/sr.json';
-// import * as translations from '../assets/ro.json';
+import * as translations from '../assets/ro.json';
 // import * as translations from '../assets/cz.json';
 // import * as translations from '../assets/sk.json';
-import * as translations from '../assets/bg.json';
+// import * as translations from '../assets/bg.json';
 // import * as productImages from '../assets/PI.json';
 // import * as productImages from '../assets/SR_product.json';
-// import * as productImages from '../assets/RO_product.json';
+import * as productImages from '../assets/RO_product.json';
 // import * as productImages from '../assets/CZ_product.json';
 // import * as productImages from '../assets/SK_product.json';
-import * as productImages from '../assets/BG_product.json';
+// import * as productImages from '../assets/BG_product.json';
 import html2canvas from 'html2canvas';
 
 
