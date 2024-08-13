d7dab24cb5d69e3a4f1f587861a65319d3f9f33c
Shajid-Henkal <shajid@karasu-online.com>
2024-08-13 14:54:17 +0600

    LV Complete!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,8 @@ import { CommonModule } from '@angular/common';
 // import * as translations from '../assets/hr.json';
 // import * as translations from '../assets/at.json';
 // import * as translations from '../assets/ba.json'; 
-import * as translations from '../assets/lt.json';
-// import * as translations from '../assets/lv.json';
+// import * as translations from '../assets/lt.json';
+import * as translations from '../assets/lv.json';
 // import * as translations from '../assets/et.json';
 // import * as translations from '../assets/si.json';
 // import * as translations from '../assets/hu.json';
@@ -33,8 +33,8 @@ import * as translations from '../assets/lt.json';
 // import * as productImages from '../assets/HR_product.json';
 // import * as productImages from '../assets/AT_product.json';
 // import * as productImages from '../assets/BA_product.json';
-import * as productImages from '../assets/LT_product.json';
-// import * as productImages from '../assets/LV_product.json';
+// import * as productImages from '../assets/LT_product.json';
+import * as productImages from '../assets/LV_product.json';
 // import * as productImages from '../assets/ET_product.json';
 // import * as productImages from '../assets/SI_product.json';
 // import * as productImages from '../assets/HU_product.json';
@@ -229,13 +229,13 @@ export class AppComponent {
         return result;
       } else {
         // return { message: 'Nie zalecamy żadnego produktu do tego zastosowania', isValid: false };
-        return { message: `Nerekomenduojame naudoti  "${translatedTile}" šiems darbams`, isValid: false };
+        return { message: `Nerekomendējam pielietot "${translatedTile}" šādai iestrādei. `, isValid: false };
       }
     } else {
       // Check if the category itself exists but has no products
       if (areaData && !(tile in areaData)) {
         // return { message: 'No products suggested for this category', isValid: false };
-        return { message: `Nerekomenduojame naudoti  "${translatedTile}" šiems darbams`, isValid: false };
+        return { message: `Nerekomendējam pielietot "${translatedTile}" šādai iestrādei. `, isValid: false };
       }
       // Default fallback if no data is available for the area or tile
       return { images: ['path/to/default_image.jpg'], isValid: true };
